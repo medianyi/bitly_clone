504ebcfebb46b7339ad2c12959904747e90ae4bd
Medianyi <vladimirmedianyi@gmail.com>
2024-01-20 17:39:55 +0300

    fix create.html links/base.html  => links/_base.html

--- links/views.py
+++ links/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Link
-
 # Create your views here.
-from .models import Link
 
 def index(request):
     links = Link.objects.all()
@@ -11,3 +9,13 @@ def index(request):
         "links": links
     }
     return render(request, 'links/index.html', context)
+
+def root_link(request, link_slug):
+    link = get_object_or_404(Link, slug=link_slug)
+    link.click() #this will increment the clicked field
+
+    return redirect(link.url)
+
+def add_link(request):
+    print(request.POST)
+    return render(request, 'links/create.html', {})
